ff81d718a65d41de76d148936479a2661efc6d95
yacamoz <88639890+yacamoz@users.noreply.github.com>
2022-01-22 20:22:13 +0900

    인기도서 목록
    API로 받아온 것을 데이터 그리드 뷰로 출력.(재업로드)

---
+++ popularityBookApi/popularityBookApi/popularityBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace popularityBookApi
+{
+    public class PopularityBook
+    {
+        public string ranking { get; set; }
+        public string bookname { get; set; }   
+        public string authors { get; set; }
+        public string publisher { get; set; }
+        public string publication_year { get; set; }
+        public string loan_count { get; set; }
+
+        public PopularityBook(string ranking, string bookname, string authors, string publisher, string publication_year, string loan_count)
+        {
+            this.ranking = ranking;
+            this.bookname = bookname;
+            this.authors = authors;
+            this.publisher = publisher;
+            this.publication_year = publication_year;
+            this.loan_count = loan_count;
+        }
+    }
+}
